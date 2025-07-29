2a90c1071a879c9148c8541fe3ec5013e8a579d4
Farzad <farzadamimi177@gmail.com>
2025-07-29 12:03:48 +0430

    object of units create

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const toggle = document.getElementById("themeToggle");
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark", toggle.checked);
 });
+const units = {
+  distance: ["meter", "kilometer", "mile", "yard", "foot"],
+  weight: ["gram", "kilogram", "pound", "ounce", "ton"],
+  temperature: ["celsius", "fahrenheit", "kelvin"],
+  time: ["second", "minute", "hour", "day"],
+  speed: ["m/s", "km/h", "mph", "knot"],
+  area: ["square meter","square kilometer","square mile","square foot","acre"],
+  volume: ["milliliter","liter","cubic meter","cubic inch","cubic foot","gallon"],
+  energy: ["joule","kilojoule","calorie","kilocalorie","watt hour","kilowatt hour"],
+  power: ["watt", "kilowatt", "horsepower"],
+  data: ["bit", "byte", "kilobyte", "megabyte", "gigabyte"],
+};
